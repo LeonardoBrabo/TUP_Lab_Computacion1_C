8572e9bb217fc71fe45087a269d8a4d5550cc847
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2023-05-25 16:22:27 -0300

    Ejercicio 5 arreglos guia1 terminado

--- Trabajos Practicos Resueltos/TP Unidad 4 - N9 - Arreglos/Ejer5TPArreglos.c
+++ Trabajos Practicos Resueltos/TP Unidad 4 - N9 - Arreglos/Ejer5TPArreglos.c
@@ -1,5 +1,5 @@
 /*• Dados dos arreglos ordenado de mayor a menor sin elementos repetidos de 10
-elementos cadauno. Se pide:
+elementos cada uno. Se pide:
 • Generar un nuevo vector ordenado que contenga a ambos vectores. Mostrar el
 resultado.
 • Mostrar el vector de menor a mayor.*/
@@ -15,7 +15,7 @@ usando un while*/
 void mostrar(int arr[],int longitud){
 	int i;
 	
-	for( i = 0; i <longitud ; i++){
+	for( i = 0; i < longitud ; i++){
 		
 		printf(" %d",arr[i]);
 	}
@@ -24,68 +24,68 @@ void mostrar(int arr[],int longitud){
 
 int main(int argc, char *argv[]) {
 	
-	int vec1[MAX] = {75,71,60,40,39,37,30,31,27,19};
-	int vec2[MAX] ={73,70,69,68,60,55,52,51,50,49};
+	int vec1[MAX] = {75,71,60,40,39,37,31,30,27,19};
+	int vec2[MAX] = {73,70,69,68,60,55,52,51,50,49};
 	int vec3[MAX*2];
 	int i = 0, j = 0, k = 0;
 	
 	
-	/*ARREGLAR CON UN CICLO WHILE PRINCIPAL, cuando termina se recorrer los demas, el principal debe ser uno de los de 10 elementos y no el resultante
+	
+	/*CON UN CICLO WHILE PRINCIPAL, cuando termina se recorrer los demas, el principal debe ser uno de los de 10 elementos y no el resultante
 	y luego termino de completar el arreglo ordenado al salir del while(se controla si termino alguno de los dos.)luego con dos while más se elige
 	dependiendo de cual falta por terminar de recorrer.*/
 	
-	while( ( i < MAX ) && ( j < MAX ) ){
+	while (i < MAX && j < MAX){
 		
-		
-		if(vec1[i] > vec2[j]){
+		if( vec1[i] > vec2[j]){
 			
-			/*caso en el que el vector1 su elemento es el mayor*/
 			vec3[k] = vec1[i];
 			i++;
+		}else if(vec2[j] > vec1[i]){
 			
-		}else{
+			vec3[k] = vec2[j];
+			j++;
 			
-			if(vec2[k] > vec1[j]){
-				
-				/*caso en el que el vector2 su elemento es el mayor*/
-				vec3[k] = vec2[j];
-				j++;
-				
-			}else{
-				
-				/*caso en el que son iguales*/
-				vec3[k] = vec1[i];
-				i++;
-				j++;
-			}
-		} 
+		}else{ /*cuando dos elementos son iguales comparando ambas lista, queda un numero de direccion en las posiciones finales.*/
+			vec3[k] = vec1[i];
+			i++;
+			j++;
+		}
 		
 		k++;
 		
 	}
 	
-	while( i < MAX  &&  k < (MAX*2)){
-		
+	while( i < MAX && k < MAX*2){
 		vec3[k] = vec1[i];
 		i++;
 		k++;
-		
 	}
 	
-	while(j < MAX && k < (MAX*2)){
-		
+	while(j < MAX && k < MAX*2){
 		vec3[k] = vec2[j];
 		j++;
 		k++;
 	}
 	
+	/*para solucionar los espacios vacios si dos elementos son iguales entre las lista y el tamanio se reduce en 1 de la lista resultante*/
+	while (k < MAX*2){
+		vec3[k] = 0;
+		k++;
+	}
 	
-	printf("\n vector 1:");
+	
+	
+	
+	printf("\n Vector 1: ");
 	mostrar(vec1,MAX);
-	printf("\n vector 2:");
+	printf("\n Vector 2: ");
 	mostrar(vec2,MAX);
-	printf("\n el vector ordenado resultante es:");
+	printf("\n Vector 3 Resultante: ");
 	mostrar(vec3,MAX*2);
+	
+	
+	
 	
 	
 	return 0;
